77383fa7d66aa60f7a4474dd6e60cd10d1a0ff1b
JatinChopra <jatinchopra2053@gmail.com>
2025-04-01 03:19:57 +0530

    map scene texture onto water surface with distortion

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import GUI from 'lil-gui';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
 import { GLTFLoader, OrbitControls, RGBELoader } from 'three/examples/jsm/Addons.js';
 
+import perlinNoise from '/src/shaders/perlinNoise.glsl?raw';
+
 const clock = new THREE.Clock();
 
 const cnvs = document.querySelector("#c") as HTMLCanvasElement;
@@ -37,7 +39,6 @@ let suzanne = new THREE.Mesh();
 
 new GLTFLoader().load('/pools.glb', (model) => {
   model.scene.traverse((obj) => {
-    console.log(obj.name);
     if (obj.name == "suzanne")
       suzanne = obj as THREE.Mesh;
     if (obj.name == "poolBig")
@@ -56,23 +57,71 @@ const orbCtrls = new OrbitControls(camera, cnvs);
 const stats = new Stats();
 document.body.appendChild(stats.dom);
 
+const renderTarget = new THREE.WebGLRenderTarget(w, h);
+renderTarget.depthTexture = new THREE.DepthTexture(w, h);
+renderTarget.depthTexture.type = THREE.FloatType;
+renderTarget.depthTexture.format = THREE.DepthFormat;
 
-function updateRendererSize() {
-
-  const currWidth = cnvs.clientWidth;
-  const currHeight = cnvs.clientHeight;
-
-  if (currWidth != w || currHeight != h) {
-    re.setSize(currWidth, currHeight, false);
-    camera.aspect = currWidth / currHeight;
-    camera.updateProjectionMatrix();
-  }
-
-
-}
+const waterUniformData = {
+  uTime: {
+    value: clock.getElapsedTime(),
+  },
+  uWindowSize: {
+    value: new THREE.Vector2(cnvs.clientWidth, cnvs.clientHeight)
+  },
+  uSceneTexture: {
+    value: renderTarget.texture
+  },
+  uDistortFreq: {
+    value: 28.0
+  },
+  uDistortAmp: {
+    value: 0.007
+  },
+};
 
 const waterPlaneGeo = new THREE.PlaneGeometry(20, 20);
 const waterMaterial = new THREE.ShaderMaterial();
+waterMaterial.uniforms = waterUniformData;
+waterMaterial.vertexShader = `
+  void main(){
+    vec3 localPos = position;
+    vec4 worldPos = modelMatrix * vec4(localPos,1.0);
+    vec4 viewPos = viewMatrix * worldPos;
+    vec4 clipPos = projectionMatrix * viewPos;
+
+    gl_Position = clipPos;
+  }
+`;
+
+waterMaterial.fragmentShader = `
+uniform float uTime;
+uniform vec2 uWindowSize;
+
+uniform sampler2D uSceneTexture;
+uniform float uDistortFreq;
+uniform float uDistortAmp;
+
+  ${perlinNoise}
+
+  void main(){
+
+    // scene below the water surface
+    vec2 sceneCoord = gl_FragCoord.xy / uWindowSize;
+
+    float offsetX = cnoise(vec2(sceneCoord.x)*uDistortFreq+uTime)*uDistortAmp;
+    float offsetY = cnoise(vec2(sceneCoord.y)*uDistortFreq+uTime)*uDistortAmp;
+
+    vec2 distortedCoord = vec2(sceneCoord.x+offsetX, sceneCoord.y+offsetY);
+
+    vec4 sceneBeneath = texture2D(uSceneTexture,distortedCoord);
+
+    vec3 color = vec3(1.0);
+    color = sceneBeneath.rgb;
+    gl_FragColor = vec4(color,1.0);
+  }
+`;
+
 const water = new THREE.Mesh(waterPlaneGeo, waterMaterial);
 water.rotateX(Math.PI * -0.5);
 scene.add(water);
@@ -89,6 +138,38 @@ visibilityFolder.add(poolSmall, 'visible').onChange((val: boolean) => {
   poolSmall.visible = val;
 }).name("Pool Small").setValue(false);
 
+const distortionFolder = gui.addFolder("Surface Distortion");
+distortionFolder.add(waterUniformData.uDistortFreq, "value", 0, 50, 0.1).name('Frequency');
+distortionFolder.add(waterUniformData.uDistortAmp, "value", 0.001, 0.01, 0.001).name('Amplitude');
+
+
+function updateWaterUniforms(time: number) {
+  waterUniformData.uTime.value = time;
+
+  waterUniformData.uWindowSize.value.x = cnvs.clientWidth;
+  waterUniformData.uWindowSize.value.y = cnvs.clientHeight;
+
+  waterUniformData.uSceneTexture.value = renderTarget.texture;
+}
+
+function updateRendererSize() {
+
+  const currWidth = cnvs.clientWidth;
+  const currHeight = cnvs.clientHeight;
+
+  if (currWidth != w || currHeight != h) {
+    re.setSize(currWidth, currHeight, false);
+    camera.aspect = currWidth / currHeight;
+    camera.updateProjectionMatrix();
+
+
+    // resize render target
+    renderTarget.setSize(currWidth, currHeight);
+
+  }
+
+
+}
 
 function animate() {
   orbCtrls.update();
@@ -98,7 +179,14 @@ function animate() {
 
   const time = clock.getElapsedTime();
 
+  updateWaterUniforms(time);
 
+  water.visible = false;
+  re.setRenderTarget(renderTarget);
+  re.render(scene, camera);
+
+  water.visible = true;
+  re.setRenderTarget(null);
   re.render(scene, camera);
   requestAnimationFrame(animate);
 };
